a7a3e6b20b881e877f982295680c6c4e10ed0e51
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-02-03 12:33:40 -0300

    primeiro desafio feito em python

---
+++ dasafio.py
@@ -0,0 +1,17 @@
+usuario = input('Qual seu nome de usuário: ')
+
+salario = flout(input('Qual o valor do seu salário: '))
+porcentagem_bonus = flot(input('Qual o valor do seu bônus: '))
+valor_fixo = 1000
+
+valor_total = valor_fixo + (salario * porcentagem_bonus / 100 )
+
+
+print(f'Olá {usuario}, tudo bem? ')
+print(f'seu valor de bônus é de {valor_total}')
+
+
+
+
+
+
