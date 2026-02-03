769ed27933c33dcdf6c6bff36d772b9052058ff3
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-02-02 23:03:11 -0300

    primeira commit sobre a primeira aula de python

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('felipe nascimento')
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+valor1 = int(input('primeiro valor: ')) 
+valor2 = int(input('Segundo valor: ')) 
+
+soma = valor1 + valor2
+print(soma)
